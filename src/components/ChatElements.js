882f0e7fef868638391ae4fbe72f16b685d9bc7c
Chris <orj.tsuk@gmail.com>
2022-07-12 06:04:46 -0400

    Adding auto-scroll to replies.

--- src/components/ChatElements.js
+++ src/components/ChatElements.js
@@ -3,31 +3,36 @@ import { useCallback } from "react"
 const ChatElements = (props) => {
 
     const chatRef = useCallback(node => {
-        if(node !== null){
+        if (node !== null) {
             node.scrollIntoView()
         }
-    },[props.data])
+    }, [props.data])
 
 
     const elems = props.data.map((item, i) => {
         if (item.ip_address === props.ip) {
             return (
-                i !== props.data.length-1 ?
-                <div key={i} className='message--container'>
-                    <p className='message--username--sent'>Anon</p>
-                    <p className='message--text sent' style={{backgroundColor:item.bgColor}}>{item.data}</p>
-                </div> :
-                <div key={i} ref={chatRef} className='message--container'>
-                <p className='message--username--sent'>Anon</p>
-                <p className='message--text sent' style={{backgroundColor:item.bgColor}}>{item.data}</p>
-            </div>
+                i !== props.data.length - 1 ?
+                    <div key={i} className='message--container'>
+                        <p className='message--username--sent'>Anon</p>
+                        <p className='message--text sent' style={{ backgroundColor: item.bgColor }}>{item.data}</p>
+                    </div> :
+                    <div key={i} ref={chatRef} className='message--container'>
+                        <p className='message--username--sent'>Anon</p>
+                        <p className='message--text sent' style={{ backgroundColor: item.bgColor }}>{item.data}</p>
+                    </div>
             )
         } else {
             return (
-                <div key={i} className='message--container'>
-                    <p className='message--username'>Anon</p>
-                    <p className='message--text recieved'style={{backgroundColor:item.bgColor}}>{item.data}</p>
-                </div>
+                i !== props.data.length - 1 ?
+                    <div key={i} className='message--container'>
+                        <p className='message--username'>Anon</p>
+                        <p className='message--text recieved' style={{ backgroundColor: item.bgColor }}>{item.data}</p>
+                    </div> :
+                    <div key={i} ref={chatRef} className='message--container'>
+                        <p className='message--username'>Anon</p>
+                        <p className='message--text recieved' style={{ backgroundColor: item.bgColor }}>{item.data}</p>
+                    </div>
             )
         }
     })
